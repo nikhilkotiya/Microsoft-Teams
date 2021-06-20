c5d85e7008d7d507231796f191734eaae6a0b2b6
nikhilkotiya <nikhilkotiya8@gmail.com>
2021-06-20 19:31:57 +0530

    first commit

--- Microsoft_Teams/settings.py
+++ Microsoft_Teams/settings.py
@@ -122,3 +122,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'accounts.User'
+
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'nikhilkotiya8@gmail.com'
+EMAIL_HOST_PASSWORD = '000@nick'
+EMAIL_PORT = 587
+EMAIL_BACKEND = (
+        "django.core.mail.backends.smtp.EmailBackend"
+    )
