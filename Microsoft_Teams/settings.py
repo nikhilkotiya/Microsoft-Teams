ffdf2f608a3d00b52aea706192fb89ed3b9a399c
Iamnikhil <51186856+nikhilkotiya@users.noreply.github.com>
2021-07-01 17:00:44 +0530

    Update settings.py

--- Microsoft_Teams/settings.py
+++ Microsoft_Teams/settings.py
@@ -139,7 +139,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1",'teams121.herokuapp.com/ ,6379)],
+            "hosts": [("127.0.0.1",'teams121.herokuapp.com',80)],
         },
     },
 }
