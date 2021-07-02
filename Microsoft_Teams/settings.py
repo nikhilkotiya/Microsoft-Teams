1df4e1b28867391ddd7e1394dccedd01a6aef387
avilgoel1 <nikhilkotiya8@gmail.com>
2021-07-02 15:30:44 +0530

    first commit

--- Microsoft_Teams/settings.py
+++ Microsoft_Teams/settings.py
@@ -1,14 +1,15 @@
+import os
 from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*d8&-=lg!_k7h!!z+b4=x&l^u2(wq-4!r*6!z9o=6-%4hkjggo'
+SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 ALLOWED_HOSTS = ['*']
-import os
+
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
