c8df1e412d0c1287e172846e49b97ec45455fb18
avilgoel1 <nikhilkotiya8@gmail.com>
2021-07-02 13:09:05 +0530

    first commit

--- Microsoft_Teams/asgi.py
+++ Microsoft_Teams/asgi.py
@@ -1,11 +1,12 @@
 import os
-
+import django
+    
 from channels.routing import ProtocolTypeRouter , URLRouter
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 import chat.routing
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Microsoft_Teams.settings')
-
+django.setup()
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     # Just HTTP for now. (We can add other protocols later.)
