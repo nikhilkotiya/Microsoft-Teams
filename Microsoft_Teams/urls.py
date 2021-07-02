9f82a0e9756f5f1a0da322e2f3058d59b257a34a
avilgoel1 <nikhilkotiya8@gmail.com>
2021-07-02 17:36:13 +0530

    first commit

--- Microsoft_Teams/urls.py
+++ Microsoft_Teams/urls.py
@@ -6,11 +6,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('',include('test_api.urls')),
     path('', include('accounts.urls')),
-    path('accounts/',include('allauth.urls')),
-    # path('feed/',include('blog.urls')),
     path('',include('chat.urls')),
-    # path('blogapi/',include('blogapi.urls')), 
     path('private/',include('app1.urls')),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
