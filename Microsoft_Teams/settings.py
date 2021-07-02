8455e577ab818f4bec0f65d00012e97b58783fee
avilgoel1 <nikhilkotiya8@gmail.com>
2021-07-02 18:11:29 +0530

    first commit

--- Microsoft_Teams/settings.py
+++ Microsoft_Teams/settings.py
@@ -111,11 +111,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 AUTHENTICATION_BACKENDS = [
@@ -144,7 +145,6 @@ CHANNEL_LAYERS = {
         },
     },
 }
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # heroku config:set DJANGO_SETTINGS_MODULE=Microsoft_Teams.settings --account teams121
 # web: daphne Microsoft_Teams.asgi:application --port $PORT --bind 0.0.0.0 -v2
 # worker: python manage.py runworker channel_layer -v2
