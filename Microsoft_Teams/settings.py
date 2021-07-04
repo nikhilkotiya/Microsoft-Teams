7f25d620707480ad737e72b80b403abde8f0a579
avilgoel1 <nikhilkotiya8@gmail.com>
2021-07-04 18:23:41 +0530

    te

--- Microsoft_Teams/settings.py
+++ Microsoft_Teams/settings.py
@@ -111,11 +111,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,"static")
-]
-STATIC_ROOT= os.path.join(BASE_DIR,"static")
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend'
