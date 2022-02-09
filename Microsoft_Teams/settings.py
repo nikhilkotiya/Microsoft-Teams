fc5c1b1c3b29cd083c2faa922565ef7e6ed7d60a
Iamnikhil <51186856+nikhilkotiya@users.noreply.github.com>
2022-02-09 15:13:27 +0530

    Update settings.py

--- Microsoft_Teams/settings.py
+++ Microsoft_Teams/settings.py
@@ -120,7 +120,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend'
