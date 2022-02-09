60b7a54b53d141baa4a8261733b98f94fb0d484f
Iamnikhil <51186856+nikhilkotiya@users.noreply.github.com>
2022-02-09 15:03:10 +0530

    Update settings.py

--- Microsoft_Teams/settings.py
+++ Microsoft_Teams/settings.py
@@ -139,11 +139,13 @@ RECAPTCHA_REQUIRED_SCORE = 0.85
 
 DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("*", 6379)],
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            #"hosts": [('127.0.0.1', 6379)],
+            "hosts" : [('redis://:Q3USyA1zpX0Jj6KTqRqaZrtjs1beQq3p@redis-19282.c246.us-east-1-4.ec2.cloud.redislabs.com:19282/0')],
         },
+        #"BACKEND":"channels.layers.InMemoryChannelLayer"
     },
 }
 # CHANNEL_LAYERS = {
