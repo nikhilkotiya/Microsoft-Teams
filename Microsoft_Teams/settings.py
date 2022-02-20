227f0fe33ff25482f3d21d13ad6374c997bb445d
nikhilkotiya <nikhilkotiya8@gmail.com>
2022-02-20 16:11:50 +0530

    first commit

--- Microsoft_Teams/settings.py
+++ Microsoft_Teams/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '*d8&-=lg!_k7h!!z+b4=x&l^u2(wq-4!r*6!z9o=6-%4hkjggo'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 import os
 # Application definition
@@ -38,7 +38,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
-#    'django.middleware.csrf.CsrfViewMiddleware',
+   'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -162,7 +162,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts" : [('redis://:M6rB1NGZO79rGu9wBuO8yq4ZIqYUZEXN@redis-19282.c246.us-east-1-4.ec2.cloud.redislabs.com:19282/0')],
+            "hosts" : [('redis://:M6rB1NGZO79rGu9wBuO8yq4ZIqYUZEXN@redis-14600.c14.us-east-1-2.ec2.cloud.redislabs.com:14600')],
         },
         # "BACKEND":"channels.layers.InMemoryChannelLayer"
     },
