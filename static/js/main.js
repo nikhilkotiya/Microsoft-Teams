b928f8eb1c0f1fc4b8eda242c5dcd264262af49e
nikhilkotiya <nikhilkotiya8@gmail.com>
2022-02-27 19:40:51 +0530

    first commit

--- static/js/main.js
+++ static/js/main.js
@@ -15,6 +15,42 @@ const menu = document.querySelector('#menu');
 //             }
 //         ]
 // };
+let iceConfiguration = {
+    "iceServers":
+        [
+            { url :' stun4.l.google.com:19302'},
+            { url: 'stunserver.org:3478'},
+            { url: 'stun:stun.l.google.com:19302' },
+            { url: 'stun:stun1.l.google.com:19302' },
+            { url: 'stun:stun2.l.google.com:19302' },
+            { url: 'stun:stun3.l.google.com:19302' },
+            // {
+            //     url: 'turn:numb.viagenie.ca',
+            //     credential: 'muazkh',
+            //     username: 'webrtc@live.com'
+            // },
+            // {
+            //     url: 'turn:192.158.29.39:3478?transport=udp',
+            //     credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+            //     username: '28224511:1379330808'
+            // },
+            // {
+            //     url: 'turn:192.158.29.39:3478?transport=tcp',
+            //     credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+            //     username: '28224511:1379330808'
+            // },
+            // {
+            //     url: 'turn:turn.bistri.com:80',
+            //     credential: 'homeo',
+            //     username: 'homeo'
+            //  },
+            //  {
+            //     url: 'turn:turn.anyfirewall.com:443?transport=tcp',
+            //     credential: 'webrtc',
+            //     username: 'webrtc'
+            // }
+        ]
+};
 window.onbeforeunload = function() {
     return "Are you sure?";
  };
