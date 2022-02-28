20ef7b24df171fe98fb797db22cd3c70b8f333fb
nikhilkotiya <nikhilkotiya8@gmail.com>
2022-02-28 17:03:16 +0530

    adas

--- static/js/main.js
+++ static/js/main.js
@@ -18,8 +18,8 @@ const menu = document.querySelector('#menu');
 let iceConfiguration = {
     "iceServers":
         [
-            { url :' stun4.l.google.com:19302'},
-            { url: 'stunserver.org:3478'},
+            // { url :'stun4.l.google.com:19302'},
+            // { url: 'stunserver.org:3478'},
             { url: 'stun:stun.l.google.com:19302' },
             { url: 'stun:stun1.l.google.com:19302' },
             { url: 'stun:stun2.l.google.com:19302' },
